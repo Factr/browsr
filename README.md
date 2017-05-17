e27caa3bc4005676353197a41e79f2bbadace64f
Adam Nelson <adam@factr.com>
2017-05-17 16:43:26 -0400

    0.3.4 Increases streams to 1000

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # browsr
 Browser extension for [Factr](https://factr.com) supporting Firefox, Safari, Chrome.  Factr is customized, real-time knowledge-sharing, letting your organization know more and act faster.
 
+## Development
+
+Run `npm run watch` to build for development.  This will put output into `output/chrome` which can be added to chrome://extensions
+
 ## Signing inline Javascript
 
 [W3 Documentation](https://www.w3.org/TR/2015/CR-CSP2-20150721/#script-src-hash-usage)
@@ -10,3 +14,7 @@ This command signs the javascript: `echo -n "alert('Hello, world.');" | openssl 
 This will return the hash for inline script.  Do not include the `<script>` tags themselves just what's inside the tags.
 
 Place the hash in kango/src/js/chrome/manifest.json and rebuild.
+
+## Build for Chrome WebStore
+
+`python kango/kango.py build .` will build the final extension.  Make sure you update `src/common/extension_info.json` with the new version so that new files are generated.
--- app/api.js
+++ app/api.js
@@ -1,4 +1,4 @@
-import { merge, forEach, map } from "lodash"
+import { merge, forEach, map } from 'lodash'
 
 //noinspection JSUnresolvedFunction
 require('es6-promise').polyfill()
@@ -9,42 +9,47 @@ function generateRoute(path, frontend) {
     return `${frontend ? config.frontendUrl : config.apiUrl}/${path}`
 }
 
-function generateHeaders(contentType = "application/json; charset=UTF-8") {
+function generateHeaders(contentType = 'application/json; charset=UTF-8') {
     const result = {}
 
-    if (contentType)
-        result["Content-Type"] = contentType
+    if (contentType) {
+        result['Content-Type'] = contentType
+    }
 
     const token = kango.storage.getItem('token')
-    if (token)
-        result["Authorization"] = `Token ${token}`
+    if (token) {
+        result['Authorization'] = `Token ${token}`
+    }
 
     return result
 }
 
-function makeApiRequest(path = {}, method = "GET", opts = {}, contentType) {
+function makeApiRequest(path = {}, method = 'GET', opts = {}, contentType) {
     const url = typeof path === 'string' ? path : path.url
     //noinspection JSUnresolvedVariable
     const newOpts = merge({}, {
         method: method,
         url: generateRoute(url, path.frontend),
-        headers: generateHeaders(contentType)
+        headers: generateHeaders(contentType),
     }, opts)
 
     let params = opts.params
-    if (params && params.constructor.name.toLowerCase() !== "formdata")
+    if (params && params.constructor.name.toLowerCase() !== 'formdata') {
         params = JSON.stringify(params)
+    }
 
     // Native XHR request
     const xhr = new XMLHttpRequest()
     xhr.open(method, newOpts.url, true)
 
     return new Promise((resolve, reject) => {
-        forEach(newOpts.headers, (headerValue, headerKey) => xhr.setRequestHeader(headerKey, headerValue))
+        forEach(newOpts.headers,
+            (headerValue, headerKey) => xhr.setRequestHeader(headerKey, headerValue))
         xhr.onreadystatechange = () => {
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 resolve(JSON.parse(xhr.responseText))
-            } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 400 || xhr.status === 0) {
+            } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 400 || xhr.status
+                === 0) {
                 reject({ noInternet: xhr.status === 0 })
             }
         }
@@ -55,12 +60,12 @@ function makeApiRequest(path = {}, method = "GET", opts = {}, contentType) {
 
 export function me() {
     const route = 'user/whoami'
-    return makeApiRequest(route, "GET")
+    return makeApiRequest(route, 'GET')
 }
 
 export function login(params) {
     const route = 'auth/token'
-    return makeApiRequest(route, "POST", { params: params })
+    return makeApiRequest(route, 'POST', { params: params })
 }
 
 export function authLinkedIn(code, redirect_uri) {
@@ -69,14 +74,14 @@ export function authLinkedIn(code, redirect_uri) {
 
     return makeApiRequest({
         url,
-        frontend: true
+        frontend: true,
     })
 }
 
 export function authGoogle(access_token) {
     return makeApiRequest({
         url: `auth/google?access_token=${access_token}&json=true`,
-        frontend: true
+        frontend: true,
     })
 }
 
@@ -84,26 +89,26 @@ export function addItemTags(streamId, itemId, tags) {
     const route = `stream/${streamId}/update_item_tags`
     const params = {
         item_id: itemId,
-        added: tags
+        added: tags,
     }
-    return makeApiRequest(route, "POST", { params: params })
+    return makeApiRequest(route, 'POST', { params: params })
 }
 
 export function postItem(streamId, params) {
     const route = `stream/${streamId}/post_item`
-    return makeApiRequest(route, "POST", { params: params })
+    return makeApiRequest(route, 'POST', { params: params })
 }
 
 export function getStreams() {
     const route = `stream/contributable`
-    return makeApiRequest(route, "GET", { params: {'mobile': true}})
+    return makeApiRequest(route, 'GET', { params: { 'limit': 1000, 'mobile': true } })
 }
 
 export function getItemFromUrl(url) {
     const route = `stream/extract_url?url=${encodeURI(url)}`
-    return makeApiRequest(route, "GET")
+    return makeApiRequest(route, 'GET')
 }
 
 export function createStream(formData) {
-    return makeApiRequest("stream", "POST", { params: formData }, false)
+    return makeApiRequest('stream', 'POST', { params: formData }, false)
 }
