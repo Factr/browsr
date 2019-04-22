0d81eb0a402a6b2b0d9a602fc1aaa9f5329b859b
Yaakov <yaakov@factr.com>
2019-04-22 16:57:26 -0400

    Update manifest

---
+++ src/static/createBeacon.js
@@ -0,0 +1,5 @@
+// Content script
+let beacon = document.createElement("div");
+// Set random string for className to avoid collision
+beacon.className = "eyda3905myu";
+document.body.appendChild(beacon);
