8abd4c7caa5991f7773cc6106befb77568a875c0
Juan Abbona <jabbona@sophilabs.com>
2020-02-20 15:28:40 -0300

    847: Delete output folder from repo.

--- output/createBeacon.js
+++
@@ -1,5 +0,0 @@
-// Content script
-let beacon = document.createElement("div");
-// Set random string for className to avoid collision
-beacon.className = "eyda3905myu";
-document.body.appendChild(beacon);
